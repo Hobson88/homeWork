8783cc14d2efc7ee5aa6f47981c85a87c8fd1c17
Hobson88 <krawczakrobert@gmail.com>
2019-02-19 09:33:43 +0100

    cosinus

--- src/zadanie5/Zadanie5.java
+++ src/zadanie5/Zadanie5.java
@@ -1,22 +1,36 @@
 package zadanie5;
+
 //Autor: Robert Krawczak
 public class Zadanie5 {
-    public static void main(String[] args) {
 
-        int amplituda = 20;
-        int ileKrokowNaPelnyKat = 20;
-        int liczbaWierszy = 60;
+    int amplituda;
+    int ileKrokowNaPelnyKat;
+    int liczbaWierszy;
+    String temp = "";
 
+    public Zadanie5(int amplituda, int ileKrokowNaPelnyKat, int liczbaWierszy) {
+        this.amplituda = amplituda;
+        this.ileKrokowNaPelnyKat = ileKrokowNaPelnyKat;
+        this.liczbaWierszy = liczbaWierszy;
+    }
 
-        //zaklęcie :)
+    public String printFunction() {
+
         for (int i = 0; i < liczbaWierszy; i++) {
             //   Math.sin(90*Math.PI/180)=1.0   ;
-            int j = (int) (amplituda * Math.sin((360 / ileKrokowNaPelnyKat) * i * Math.PI / 180) + amplituda+1);
+            int j = (int) (amplituda * Math.cos((360 / ileKrokowNaPelnyKat) * i * Math.PI / 180) + amplituda + 1);
             for (int k = 0; k < j; k++) {
-                System.out.print("*");
+                temp = temp.concat("*");
             }
-            System.out.println();
+            temp = temp.concat("\n");
         }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        Zadanie5 cosinusik = new Zadanie5(10, 20, 40);
+
+        System.out.println(cosinusik.printFunction() );
 
     }
 }
